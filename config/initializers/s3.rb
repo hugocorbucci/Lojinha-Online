17838c043e7fb86d25fe27237528fb4a15b53cb5
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2015-03-16 00:35:40 -0400

    Upgrading aws sdk and other minor gems or dev gems

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,4 +1,2 @@
-AWS.config(
-  :access_key_id     => ENV['S3_KEY'],
-  :secret_access_key => ENV['S3_SECRET']
-)
+Aws.config[:access_key_id] = ENV['S3_KEY']
+Aws.config[:secret_access_key] = ENV['S3_SECRET']
